46421ffb63d9b139b477651b236a7adec5f849e8
Richard Janis Goldschmidt <github@aberrat.io>
2025-09-08 19:24:49 +0200

    explain the eyre error hook in tempo-commonware

--- bin/tempo-commonware/src/main.rs
+++ bin/tempo-commonware/src/main.rs
@@ -4,6 +4,13 @@
 use tempo_commonware_node::cli;
 
 fn main() {
+    // XXX: ensures that the error source chain is preserved in
+    // tracing-instrument generated error events. That is, this hook ensures
+    // that functions instrumented like `#[instrument(err)]` will emit an event
+    // that contains the entire error souce chain.
+    //
+    // TODO: Can remove this if https://github.com/tokio-rs/tracing/issues/2648
+    // ever gets addressed.
     tempo_eyre::install()
         .expect("must install the eyre error hook before constructing any eyre reports");
     if let Err(err) = cli::run() {
