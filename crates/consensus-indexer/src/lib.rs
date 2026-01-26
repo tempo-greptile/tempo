4753781d99489157623eb57726ae74ef07ec509a
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2026-01-26 19:55:26 +0100

    feat(consensus): add external consensus certificate indexer
    This will be used to guard our validator nodes against traffic, and
    eventually will be used for RPC nodes to follow.
    
    The indexer stores finalizations (no notarizations for now) in SQLite,
    which is fine, given that resyncing an indexer takes around 15 minutes
    and we do not expect to lose the data.
    
    The indexer also verifies the finalizations against the network
    identity (and supports identity transitions).

---
+++ crates/consensus-indexer/src/lib.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
+pub mod db;
+pub mod feed;
+pub mod identity;
+pub mod indexer;
+pub mod state;
+pub mod store;
+
+#[cfg(test)]
+mod tests;
